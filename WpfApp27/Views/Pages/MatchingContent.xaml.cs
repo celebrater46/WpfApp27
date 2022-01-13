3b82c85d5fbad34724dfaf5a6c81225a724b1cd6
celebrater <tkgm45nbchk@gmail.com>
2022-01-13 18:39:00 +0900

    added user controls

---
+++ WpfApp27/Views/Pages/MatchingContent.xaml.cs
@@ -0,0 +1,12 @@
+﻿using System.Windows.Controls;
+
+namespace WpfApp27.Views.Pages
+{
+    public partial class MatchingContent : UserControl
+    {
+        public MatchingContent()
+        {
+            InitializeComponent();
+        }
+    }
+}
